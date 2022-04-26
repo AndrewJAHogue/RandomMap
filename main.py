ce0cb87797acb5cfd29cc252917a518b353848d4
AndrewJAHogue <andrewjahogue@gmail.com>
2022-04-25 19:55:39 -0500

    Switching to a more professional format.

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
+import sys
 from pygame.math import Vector2
 import math
 import pygame
 import numpy as np
+from settings import *
 
-pygame.init()
-game_map = np.full((16, 16),0)
-game_map[0] = 1
-game_map[15] = 1
-game_map[:,0] = 1
-game_map[:,15] = 1
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode(SCREEN_SIZE)
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+    def run(self):
+        while self.running:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit() 
+                    sys.exit()
+            
+            self.surface.fill(COLORS['white'])
+            pygame.display.update()
+            self.clock.tick(FPS)
 
-screen_size = (800, 800)
-surface = pygame.display.set_mode(screen_size)
+if __name__ == '__main__':
+    game = Game()
+    game.run()
 
-screen_map_ratio = [int(screen_size[0] / 16), int(screen_size[1] / 16)]
-print(screen_map_ratio)
 
-SCREEN_COLORS = {
-    "white": (255, 255, 255),
-    "black": (0, 0, 0)
-} 
-
-game_map[3, 5] = 2
-hero_position = np.array([3, 5])
-
-print(game_map)
-running = True
-while running:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            running = False
-            pygame.quit() 
-    surface.fill(SCREEN_COLORS['white'])
-
-    for eye,y in enumerate(game_map):
-        for ex, x in enumerate(y):
-            if x == 1:
-                black_rect = pygame.Rect((ex* 20, eye* 20),(20, 20))
-                pygame.draw.rect(surface, (0,0,0,0),black_rect)
-
-    pygame.display.flip()

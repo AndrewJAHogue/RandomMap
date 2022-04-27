a810f318d88daae302a87a6acff4a014ddc07011
AndrewJAHogue <andrewjahogue@gmail.com>
2022-04-26 20:42:37 -0500

    Created method to randomize the map

--- level.py
+++ level.py
@@ -26,3 +26,20 @@ class Level():
                 if col == 1:
                     Tile((x, y), COLORS['black'])
     
+    # Randomization of the world map
+    def GenerateMap(self):
+        for row_i, row in enumerate(world_map):
+            for col_i, col in enumerate(row):
+                if self.make_map:
+                    if col == 1 and row_i > 0 and col_i > 0:
+                        chance = np.random.choice([0,1])
+                        world_map[col_i, row_i] = chance
+        
+
+        # Ensure there's an entrance
+        world_map[0,0] = 0
+        world_map[1,0] = 0
+        world_map[1,1] = 0
+        # Ensure there's an exit
+        world_map[world_map.shape[0] -1, world_map.shape[1] - 2:] = 0
+        self.make_map = False
